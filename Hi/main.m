82cb0947e2b758d3bd462f17d7785f900b4dd639
Marc Palluat de Besset <marc.palluatdebesset@hibu.com>
2015-02-09 11:40:22 +0000

    Initial Commit

---
+++ Hi/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Hi
+//
+//  Created by Marc Palluat de Besset on 09/02/2015.
+//  Copyright (c) 2015 hibu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
